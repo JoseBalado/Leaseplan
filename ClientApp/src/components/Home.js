2ad853c8c0465e39b42d38976cf7688cbf796448
Jose Balado <JoseBalado@users.noreply.github.com>
2021-01-10 18:55:22 +0100

    Add README.md and copy that content to the Home.js file

--- ClientApp/src/components/Home.js
+++ ClientApp/src/components/Home.js
@@ -6,20 +6,25 @@ export class Home extends Component {
   render () {
     return (
       <div>
-        <h1>Hello, world!</h1>
+        <h1>Leaseplan</h1>
         <p>Welcome to your new single-page application, built with:</p>
         <ul>
           <li><a href='https://get.asp.net/'>ASP.NET Core</a> and <a href='https://msdn.microsoft.com/en-us/library/67ef8sbd.aspx'>C#</a> for cross-platform server-side code</li>
           <li><a href='https://facebook.github.io/react/'>React</a> for client-side code</li>
           <li><a href='http://getbootstrap.com/'>Bootstrap</a> for layout and styling</li>
         </ul>
-        <p>To help you get started, we have also set up:</p>
+        <p>Tested with:</p>
         <ul>
-          <li><strong>Client-side navigation</strong>. For example, click <em>Counter</em> then <em>Back</em> to return here.</li>
-          <li><strong>Development server integration</strong>. In development mode, the development server from <code>create-react-app</code> runs in the background automatically, so your client-side resources are dynamically built on demand and the page refreshes when you modify any file.</li>
-          <li><strong>Efficient production builds</strong>. In production mode, development-time features are disabled, and your <code>dotnet publish</code> configuration produces minified, efficiently bundled JavaScript files.</li>
+          <li>Linux Mint 20</li>
+          <li>.NET 5.0.101</li>
+          <li>Google Chrome Version 87.0.4280.141 (Official Build) (64-bit)</li>
         </ul>
-        <p>The <code>ClientApp</code> subdirectory is a standard React application based on the <code>create-react-app</code> template. If you open a command prompt in that directory, you can run <code>npm</code> commands such as <code>npm test</code> or <code>npm install</code>.</p>
+        <p>To run the Leaseplan application:</p>
+        <p>cd into the folder "Leaseplan/" and execute the command <code>dotnet run</code></p>
+        <ul>
+          <li><code>$ cd Leaseplan/</code></li>
+          <li><code>$ dotnet run</code></li>
+        </ul>
       </div>
     );
   }
---
+++ README.md
@@ -0,0 +1,13 @@
+## Leaseplan
+
+### Tested with:
+    Linux Mint 20
+    .NET 5.0.101
+    Google Chrome Version 87.0.4280.141 (Official Build) (64-bit)
+
+### To run the Leaseplan application
+
+cd into the folder "Leaseplan/" and execute the command dotnet run
+
+    $ cd Leaseplan/
+    $ dotnet run
